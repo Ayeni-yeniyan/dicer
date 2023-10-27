b31824ae65c6981e1a8e344216b11aab33764e93
Ayeni-yeniyan <connectwithayeni@gmail.com>
2023-10-27 11:53:14 +0100

    A dice rolling app!

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,57 +9,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color background = Colors.red.shade900;
     return MaterialApp(
-      home: Scaffold(
-          backgroundColor: background,
-          appBar: AppBar(
-            backgroundColor: background,
-            centerTitle: true,
-            title: Text('DICER!',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          ),
-          body: DicePage()),
+      home: Scaffold(backgroundColor: Colors.black, body: QuestionPage()),
     );
   }
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  State<DicePage> createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 2;
-  int rightDiceNumber = 4;
-  void diceNumber() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
+class QuestionPage extends StatelessWidget {
+  const QuestionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
+    return SafeArea(
+      child: Column(
         children: [
+          Expanded(flex: 5, child: Text('Are the questions displaying?')),
           Expanded(
-              child: TextButton(
-            style: TextButton.styleFrom(padding: EdgeInsets.all(16)),
-            onPressed: () {
-              diceNumber();
-            },
-            child: Image.asset('assets/images/dice$leftDiceNumber.png'),
-          )),
+            flex: 1,
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
+              child: Text('TRUE'),
+            ),
+          ),
           Expanded(
-              child: TextButton(
-            style: TextButton.styleFrom(padding: const EdgeInsets.all(16)),
-            onPressed: () {
-              diceNumber();
-            },
-            child: Image.asset('assets/images/dice$rightDiceNumber.png'),
-          ))
+            flex: 1,
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              child: Text('FALSE'),
+            ),
+          ),
         ],
       ),
     );
